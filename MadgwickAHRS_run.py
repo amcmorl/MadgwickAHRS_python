2a667f61a4f17a8c3a4a0588da93b695bc151ccf
Angus McMorland <amcmorl@gmail.com>
2017-02-24 12:15:18 +1300

    Fixed parameter order to g first

--- MadgwickAHRS_run.py
+++ MadgwickAHRS_run.py
@@ -5,7 +5,7 @@ ax = 0.
 ay = 2.
 az = 5.
 
-gx = -06.2
+gx = -6.2
 gy = 7.8
 gz = 0.
 
@@ -13,7 +13,7 @@ mx = 0.0
 my = 0.
 mz = 24.
 
-MadgwickAHRS.MadgwickAHRSupdate(ax, ay, az, gx, gy, gz, mx, my, mz)
+MadgwickAHRS.MadgwickAHRSupdate(gx, gy, gz, ax, ay, az, mx, my, mz)
 cv = MadgwickAHRS.cvar
 q0, q1, q2, q3 = cv.q0, cv.q1, cv.q2, cv.q3
 print(q0, q1, q2, q3)
